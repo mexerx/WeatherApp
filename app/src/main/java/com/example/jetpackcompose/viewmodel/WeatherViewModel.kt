301abe0c8f9a36784e51d9dc164eeec149dbd650
mexerx <mex.teckel@gmail.com>
2024-12-29 15:32:50 +0100

    full Project

---
+++ app/src/main/java/com/example/jetpackcompose/viewmodel/WeatherViewModel.kt
@@ -0,0 +1,89 @@
+package com.example.jetpackcompose.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.jetpackcompose.api.WeatherApiService
+import com.example.jetpackcompose.data.ForecastItem
+import com.example.jetpackcompose.data.WeatherData
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class WeatherViewModel : ViewModel() {
+
+    // State Flows for weather data
+    private val _currentWeather = MutableStateFlow<WeatherData?>(null)
+    val currentWeather: StateFlow<WeatherData?> = _currentWeather
+
+    private val _forecast = MutableStateFlow<List<ForecastItem>>(emptyList())
+    val forecast: StateFlow<List<ForecastItem>> = _forecast
+
+    private val _iconUrl = MutableStateFlow<String?>(null)
+    val iconUrl: StateFlow<String?> get() = _iconUrl
+
+    private val _errorMessage = MutableStateFlow<String?>(null)
+    val errorMessage: StateFlow<String?> get() = _errorMessage
+
+
+    /**
+     * Fetch weather data
+     *
+     * @param city
+     * @param apiKey
+     */
+    fun fetchWeatherData(city: String, apiKey: String) {
+        viewModelScope.launch {
+            try {
+                val weatherResponse = WeatherApiService.fetchWeather(city, apiKey)
+                if (weatherResponse != null) {
+                    _currentWeather.value = weatherResponse
+                    fetchWeatherIcon(weatherResponse.weather.firstOrNull()?.icon.orEmpty())
+                    _errorMessage.value = null
+                } else {
+                    _errorMessage.value = "Failed to fetch weather. Please check your API key or city name."
+                }
+            } catch (e: Exception) {
+                _errorMessage.value = "An error occurred: ${e.localizedMessage}"
+            }
+        }
+    }
+
+    /**
+     * Fetch forecast data
+     *
+     * @param city
+     * @param apiKey
+     */
+
+    fun fetchForecastData(city: String, apiKey: String) {
+        viewModelScope.launch {
+            try {
+                val forecastResponse = WeatherApiService.fetchForecast(city, apiKey)
+                if (forecastResponse != null) {
+                    _forecast.value = forecastResponse.list
+
+
+                    _errorMessage.value = null
+                } else {
+                    _errorMessage.value = "Failed to fetch forecast. Please check your API key or city name."
+                }
+            } catch (e: Exception) {
+                _errorMessage.value = "An error occurred: ${e.localizedMessage}"
+            }
+
+        }
+
+
+    }
+
+    /**
+     * Fetch weather icon
+     *
+     * @param iconId
+     */
+    private fun fetchWeatherIcon(iconId: String) {
+        if (iconId.isNotEmpty()) {
+            _iconUrl.value = "https://openweathermap.org/img/wn/$iconId@2x.png"
+        }
+    }
+}
